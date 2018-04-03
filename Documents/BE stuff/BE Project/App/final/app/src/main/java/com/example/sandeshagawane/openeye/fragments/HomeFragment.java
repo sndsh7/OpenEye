07df950a265321709684779737232a9b32858e11
sndsh7 <sndshagawane412@gmail.com>
2018-04-03 22:09:53 +0530

    retrive posts from firebase firestore and show on home_fragment like button with fully working

--- Documents/BE stuff/BE Project/App/final/app/src/main/java/com/example/sandeshagawane/openeye/fragments/HomeFragment.java
+++ Documents/BE stuff/BE Project/App/final/app/src/main/java/com/example/sandeshagawane/openeye/fragments/HomeFragment.java
@@ -3,12 +3,26 @@ package com.example.sandeshagawane.openeye.fragments;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.sandeshagawane.openeye.BlogPost;
+import com.example.sandeshagawane.openeye.BlogRecyclerAdapter;
 import com.example.sandeshagawane.openeye.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentChange;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -16,7 +30,14 @@ import com.example.sandeshagawane.openeye.R;
 public class HomeFragment extends Fragment {
 
     private RecyclerView post_list_view;
+    private List<BlogPost> blog_list;
 
+    private FirebaseFirestore firebaseFirestore;
+    private FirebaseAuth firebaseAuth;
+    private BlogRecyclerAdapter blogRecyclerAdapter;
+
+    private DocumentSnapshot lastVisible;
+    private Boolean isFirstPageFirstLoad = true;
 
     public HomeFragment() {
         // Required empty public constructor
@@ -29,11 +50,121 @@ public class HomeFragment extends Fragment {
 
         View view = inflater.inflate(R.layout.fragment_home, container, false);
 
-        post_list_view = getActivity().findViewById(R.id.post_list_view);
+        blog_list = new ArrayList<>();
+        post_list_view = view.findViewById(R.id.post_list_view);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        blogRecyclerAdapter = new BlogRecyclerAdapter(blog_list);
+        post_list_view.setLayoutManager(new LinearLayoutManager(container.getContext()));
+        post_list_view.setAdapter(blogRecyclerAdapter);
+        post_list_view.setHasFixedSize(true);
+
+        if(firebaseAuth.getCurrentUser() != null) {
+
+            firebaseFirestore = FirebaseFirestore.getInstance();
+
+            post_list_view.addOnScrollListener(new RecyclerView.OnScrollListener() {
+                @Override
+                public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
+                    super.onScrolled(recyclerView, dx, dy);
+
+                    Boolean reachedBottom = !recyclerView.canScrollVertically(1);
+
+                    if(reachedBottom){
+
+                        loadMorePost();
+
+                    }
+
+                }
+            });
+
+            Query firstQuery = firebaseFirestore.collection("Posts").orderBy("timestamp", Query.Direction.DESCENDING).limit(3);
+            firstQuery.addSnapshotListener(getActivity(), new EventListener<QuerySnapshot>() {
+                @Override
+                public void onEvent(QuerySnapshot documentSnapshots, FirebaseFirestoreException e) {
+
+                    if (!documentSnapshots.isEmpty()) {
+
+                        if (isFirstPageFirstLoad) {
+
+                            lastVisible = documentSnapshots.getDocuments().get(documentSnapshots.size() - 1);
+                            blog_list.clear();
+
+                        }
+
+                        for (DocumentChange doc : documentSnapshots.getDocumentChanges()) {
+
+                            if (doc.getType() == DocumentChange.Type.ADDED) {
+
+                                String blogPostId = doc.getDocument().getId();
+                                BlogPost blogPost = doc.getDocument().toObject(BlogPost.class).withId(blogPostId);
+
+                                if (isFirstPageFirstLoad) {
+
+                                    blog_list.add(blogPost);
+
+                                } else {
+
+                                    blog_list.add(0, blogPost);
+
+                                }
+
+
+                                blogRecyclerAdapter.notifyDataSetChanged();
+
+                            }
+                        }
+
+                        isFirstPageFirstLoad = false;
+
+                    }
+
+                }
+
+            });
+
+        }
 
         // Inflate the layout for this fragment
         return view;
+    }
 
+    public void loadMorePost(){
+
+        if(firebaseAuth.getCurrentUser() != null) {
+
+            Query nextQuery = firebaseFirestore.collection("Posts")
+                    .orderBy("timestamp", Query.Direction.DESCENDING)
+                    .startAfter(lastVisible)
+                    .limit(3);
+
+            nextQuery.addSnapshotListener(getActivity(), new EventListener<QuerySnapshot>() {
+                @Override
+                public void onEvent(QuerySnapshot documentSnapshots, FirebaseFirestoreException e) {
+
+                    if (!documentSnapshots.isEmpty()) {
+
+                        lastVisible = documentSnapshots.getDocuments().get(documentSnapshots.size() - 1);
+                        for (DocumentChange doc : documentSnapshots.getDocumentChanges()) {
+
+                            if (doc.getType() == DocumentChange.Type.ADDED) {
+
+                                String blogPostId = doc.getDocument().getId();
+                                BlogPost blogPost = doc.getDocument().toObject(BlogPost.class).withId(blogPostId);
+                                blog_list.add(blogPost);
+
+                                blogRecyclerAdapter.notifyDataSetChanged();
+                            }
+
+                        }
+                    }
+
+                }
+            });
+
+        }
 
     }
 
